c8c0c6524e2c1f029f32b0a7e3606fa82d2540d7
ColMugX <colmugx@qq.com>
2017-08-15 00:33:49 +0800

    Site updated: 2017-08-15 00:33:49

--- script/nlvi.js
+++ script/nlvi.js
@@ -33,12 +33,18 @@
         
         onView() {
             var title = document.title
+            var tme
             document.addEventListener('visibilitychange', function() {
                 var sct = Math.floor($(window).scrollTop() / ($(document).height() - $(window).height()) * 100)
                 if (document.hidden) {
+                    clearTimeout(tme)
+                    if (sct <= 0) sct = 0
                     document.title = 'Read '+sct+'% · '+document.title
                 } else {
-                    document.title = title
+                    document.title = 'Welcome Back · ' + title
+                    tme = setTimeout(function() {
+                        document.title = title
+                    }, 3000)
                 }
             })
         },
