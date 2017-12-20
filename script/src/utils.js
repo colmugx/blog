480c13cc1ed3b345f805c48d0bb769ee2f6681b5
ColMugX <colmugx@qq.com>
2017-12-20 15:11:41 +0800

    Site updated: 2017-12-20 15:11:40

--- script/src/utils.js
+++ script/src/utils.js
@@ -1,28 +1,4 @@
 Nlvi.utils = {
-  back2top: function() {
-    var scrollTop = Nlvi.tools.scroll(window);
-    scrollTop(function(sct) {
-      if (sct > 110) {
-        Nlvi.tools.opreateClass('#backtop', 'bloom', 'add');
-        Nlvi.tools.opreateClass('#backtop', 'dead', 'remove');
-      } else {
-        Nlvi.tools.opreateClass('#backtop', 'dead', 'add');
-        Nlvi.tools.opreateClass('#backtop', 'bloom', 'remove');
-      }
-      var scrollPercentRounded = Math.floor(
-        sct 
-        / ($(document).height() - $(this).height())
-        * 100
-      );
-      $('#scrollpercent').html(scrollPercentRounded);
-    });
-    $('.toTop').on('click', function () {
-      $('html, body').animate({
-        scrollTop: 0
-      });
-    });
-  },
-
   mobileHeader: function() {
     function resetMobileMenu() {
       $('.mobile-header').css({
